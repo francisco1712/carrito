73d9dd843d70ce0b6409a3f4c1adc94f9b1941b1
francisco1712 <59690182+francisco1712@users.noreply.github.com>
2020-05-12 20:16:27 +0200

    Update config.php

--- global/config.php
+++ global/config.php
@@ -1,3 +1,4 @@
+//DEFINE LA CLAVE Y EL CODIGO PARA VER LOS PRODUCTOS
 <?php
 define("KEY","xbox");
 define("COD","AES-128-ECB");
@@ -6,10 +7,4 @@ define("SERVIDOR", 'localhost');
 define('BD', 'php_carrito'); //localhost
 define('PASSWD', ''); //localhost
 define('User', 'root') //localhost
-//define('BD', '3422628_Zykrex'); //hosting award
-//define('PASSWD', 'Z80e[5^21(f2nJzf'); //hosting award
-//define('User', '3422628_zykrex') //hosting award
-//define('BD', 'id12805097_zykrex'); //hosting 000webhost
-//define('PASSWD', 'KOFXPnZ<Zf+8RM$L'); //hosting 000webhost
-//define('User', 'id12805097_administradorcarro') //hosting 000webhost
 ?>
